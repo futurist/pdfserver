d12b1da69fcfad4a9b0ec9a805c3c93613aaf41e
ly <26634873@qq.com>
2015-07-16 18:06:20 +0800

    first

---
+++ app.js
@@ -0,0 +1,81 @@
+var qiniu = require('qiniu');
+var path = require('path');
+var fs = require('fs');
+
+var log = require('tracer').console({
+    transport : function(data) {
+        console.log(data.output);
+        fs.open('./log.txt', 'a', 0666, function(e, id) {
+            fs.write(id, data.output+"\n", null, 'utf8', function() {
+                fs.close(id, function() {
+                });
+            });
+        });
+    }
+});
+
+
+qiniu.conf.ACCESS_KEY = '2hF3mJ59eoNP-RyqiKKAheQ3_PoZ_Y3ltFpxXP0K';
+qiniu.conf.SECRET_KEY = 'xvZ15BIIgJbKiBySTV3SHrAdPDeGQyGu_qJNbsfB';
+
+var formatDate = function(format) {
+	d = new Date();
+    var yyyy = d.getFullYear().toString();
+    format = format.replace(/yyyy/g, yyyy)
+    var mm = (d.getMonth()+1).toString(); 
+    format = format.replace(/mm/g, (mm[1]?mm:"0"+mm[0]));
+    var dd  = d.getDate().toString();
+    format = format.replace(/dd/g, (dd[1]?dd:"0"+dd[0]));
+    var hh = d.getHours().toString();
+    format = format.replace(/hh/g, (hh[1]?hh:"0"+hh[0]));
+    var ii = d.getMinutes().toString();
+    format = format.replace(/ii/g, (ii[1]?ii:"0"+ii[0]));
+    var ss  = d.getSeconds().toString();
+    format = format.replace(/ss/g, (ss[1]?ss:"0"+ss[0]));
+    return format;
+};
+
+
+if(process.argv.length<3) process.exit();
+
+var client = process.argv[2];
+var title = process.argv[3];
+var file = process.argv[4];
+
+
+var ext = path.extname(file);
+var saveFile = path.basename(file); //formatDate('yyyymmdd-hhiiss') + Math.random().toString().slice(1,5) + ext;
+
+var responseBody = 
+{
+	"key":"$(key)",
+	"hash":"$(hash)",
+	"imageWidth":"$(imageInfo.width)",
+	"imageHeight":"$(imageInfo.height)",
+	type:"$(type)",
+	fname:"$(fname)",
+	fsize:"$(fsize)"
+};
+
+var putPolicy = new qiniu.rs.PutPolicy(
+	'bucket01',
+	null,
+	null,
+	null,
+	JSON.stringify( responseBody )
+);
+
+var uptoken = putPolicy.token();
+
+log.log( uptoken,saveFile, file, JSON.stringify(process.argv) );
+
+qiniu.io.putFile(uptoken, saveFile, file, null, function(err, ret) {
+  log.log(err, ret);
+});
+
+
+sleep(15000);
+function sleep(sleepTime) {
+for(var start = +new Date; +new Date - start <= sleepTime; ) { } 
+}
+
